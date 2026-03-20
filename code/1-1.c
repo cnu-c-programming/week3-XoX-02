f43af0bc4d2cc03ace2ffe7ab12f8c3dff1faf16
XoX-02 <boy3898@naver.com>
2026-03-20 12:19:04 +0900

    update

--- code/1-1.c
+++ code/1-1.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+int add(int a, intb){return a+b;}
+int sub(int a, intb){return a-b;}
+int mul(int a, intb){return a*b;}
+int div(int a, intb){return a/b;}
 
 int main() {
+    int a =10, b = 5;
 
+    printf("%d + %d = %d",a,b,add(a,b));
+    printf("%d - %d = %d",a,b,sub(a,b));
+    printf("%d * %d = %d",a,b,mul(a,b));
+    printf("%d / %d = %d",a,b,div(a,b));
     return 0;
 }
--- code/3-5.c
+++ code/3-5.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+void my_sum(char sig,int n,...){
+    va_list ap;
+
+    va_start(ap,n);
+
+    if(sig == 'S'){
+        char str[100] = "";
+        for(int i = 0; i < n-1; i++){
+            strcat(str,va_arg(ap,char*));
+            strcat(str," ");
+        }
+        strcat(str,va_arg(ap,char*));
+        printf("%s\n",str);
+    }
+    else if(sig == 'C'){
+        char str[100] = "";
+        for(int i = 0; i < n; i++){
+            str[i] = (char)va_arg(ap,int);
+        }
+        str[n] = '\0';
+        printf("%s",str);
+    }
+    else if(sig == 'D'){
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += va_arg(ap,int);
+        }
+        printf("%d\n",sum);
+    }
+}
 int main() {
+    my_sum('S', 2, "Hello", "World");
+    my_sum('C', 3, 'C', 'N', 'U');
+    my_sum('D', 4, 10, 20, 30, 40);
 
     return 0;
 }
